1718a054259100a95635a0b55937d27d826b8bf7
erick <834424581@qq.com>
2018-04-16 08:01:21 +0800

    发送http请求及关键字判断

--- fisher.py
+++ fisher.py
@@ -1,24 +1,19 @@
 from flask import Flask,make_response
+from helper import is_isbn_or_key
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 
-@app.route('/hello')
-def hello():
-    #基于类的视图（即插视图）
-    #返回status code 200，404，301
-    #返回content-type  http headers
-    #content-type = text/html
-    # 返回一个Response对象
-    headers = {
-        'content-type':'text/plain',
-        'location':'http://www.bing.com'
-    }
-    # response = make_response('<html></html>',404)
-    # response.headers = headers
-    return '<html></html>',301,headers
-    #return '<html></html>'
+@app.route('/book/search/<q>/<page>')
+def search(q,page):
+    '''
+    q :普通关键词和isbn
+    page
+    '''
+    isbn_or_key = is_isbn_or_key(q)
+
+    pass
 
 #app.add_url_rule('/hello',view_func=hello)
 
---
+++ http.py
@@ -0,0 +1,10 @@
+import requests
+
+'''http://t.yushu.im/v2/book/isbn/9787501524044'''
+class HTTP:
+
+    def get(self,url,return_json=True):
+        r = requests.get(url)
+        if r.status_code != 200:
+            return {} if return_json else ''
+        return r.json() if return_json else r.text
