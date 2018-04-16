3292b107db42fe79201720b3a21f53042fc9cbcb
erick <834424581@qq.com>
2018-04-17 07:38:48 +0800

    利用蓝图拆分文件

--- app/web/book.py
+++ app/web/book.py
@@ -1,9 +1,9 @@
-from flask import jsonify, Blueprint
+from flask import jsonify
 
+from . import web
 from helper import is_isbn_or_key
 from yushu_book import YuShuBook
 
-web = Blueprint('web',__name__)
 
 @web.route('/book/search/<q>/<page>')
 def search(q,page):
