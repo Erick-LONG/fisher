e61048eedcbd88b3cd7efbcdbe1d7fe106f5ac21
erick <834424581@qq.com>
2018-04-17 08:08:22 +0800

    WTform参数验证

--- app/web/book.py
+++ app/web/book.py
@@ -1,20 +1,28 @@
-from flask import jsonify
+from flask import jsonify, request
 
+from app.forms.book import SearchForm
 from . import web
 from helper import is_isbn_or_key
 from yushu_book import YuShuBook
 
 
-@web.route('/book/search/<q>/<page>')
-def search(q,page):
+@web.route('/book/search')
+def search():
     '''
     q :普通关键词和isbn
     page
+    ?q=金庸&page=1
     '''
-    isbn_or_key = is_isbn_or_key(q)
-    if isbn_or_key == 'isbn':
-        result = YuShuBook.search_by_isbn(q)
+    form = SearchForm(request.args)
+    if form.validate():
+        q = form.q.data.strip()
+        page = form.page.data
+        isbn_or_key = is_isbn_or_key(q)
+        if isbn_or_key == 'isbn':
+            result = YuShuBook.search_by_isbn(q)
+        else:
+            result = YuShuBook.search_by_keyword(q)
+            #dict序列化 API
+        return jsonify(result)
     else:
-        result = YuShuBook.search_by_keyword(q)
-        #dict序列化 API
-    return jsonify(result)
+        return jsonify({'msg':'参数押证失败'})
