094a44a3a4453357e298acc6473979e4b3cb62d0
erick <834424581@qq.com>
2018-04-18 08:11:35 +0800

    利用sqlalchemy新建数据表

--- app/spider/yushu_book.py
+++ app/spider/yushu_book.py
@@ -1,4 +1,6 @@
-from http import HTTP
+from flask import current_app
+
+from app.libs.http import HTTP
 
 
 class YuShuBook:
@@ -12,7 +14,11 @@ class YuShuBook:
         return result
 
     @classmethod
-    def search_by_keyword(cls,keyword,count=15,start=0):
-        url = cls.key_word_url.format(keyword,count,start)
+    def search_by_keyword(cls,keyword,page=1):
+        url = cls.key_word_url.format(keyword,current_app.config['PER_PAGE'],cls.calculate_start(page))
         result = HTTP.get(url)
         return result
+
+    @staticmethod
+    def calculate_start(page):
+        return (page-1)* current_app.config['PER_PAGE']
