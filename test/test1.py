951e6db9573e641c2b1657a23732d9e9818f1774
erick <834424581@qq.com>
2018-04-19 22:27:42 +0800

    with改写应用上下文

--- test/test1.py
+++ test/test1.py
@@ -7,10 +7,15 @@ app = Flask(__name__)
 # Request RequestContext 代理request请求对象，先检查应用上下文的栈顶，如果没有，把app推送到栈中
 #离线应用，单元测试，需要手动推入应用上下文，如果有请求上线文，flask会自动推入，否则，手动推入
 
-ctx = app.app_context() #获取应用上下文
-ctx.push() #入栈
+# ctx = app.app_context() #获取应用上下文
+# ctx.push() #入栈
+#
+# a = current_app
+# d = current_app.config['DEBUG']
+#
+# ctx.pop() #出栈
 
-a = current_app
-d = current_app.config['DEBUG']
-
-ctx.pop() #出栈
+# with
+with app.app_context():
+    a = current_app
+    d = current_app.config['DEBUG']
