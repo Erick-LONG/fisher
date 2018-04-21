b230019bc570b22cf4d31d02e3e29b5bcf58f150
erick <834424581@qq.com>
2018-04-21 14:59:12 +0800

    线程隔离的意义及viewModel的设计

--- test/test5.py
+++ test/test5.py
@@ -2,19 +2,21 @@
 import threading
 
 import time
-from werkzeug.local import Local
+from werkzeug.local import Local,LocalStack
 
-my_obj = Local()
-my_obj.b = 1
+my_stack = LocalStack()
+my_stack.push(1)
 
 def worker():
-    my_obj.b = 2
-    print('new thread b is ' + str(my_obj.b)) #2
+    my_stack.push(2)
+    print('new thread b is ' + str(my_stack.top)) #2
 
 
 new_t = threading.Thread(target=worker,name='qq_thread')
 new_t.start()
 time.sleep(1)
 
-print('main thread b is' + str(my_obj.b))
+print('main thread b is' + str(my_stack.top))
 #1
+
+#使用线程隔离的意义在于：使当前线程能正确的引用到他自己所创建的对象，而不是引用到其他线程所创建的对象
