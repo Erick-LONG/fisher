a6463d7a8ddec89ade0cc7400f9afae49b64defb
erick <834424581@qq.com>
2018-04-21 21:16:07 +0800

    重构视图函数和数据获取函数

--- app/web/book.py
+++ app/web/book.py
@@ -3,7 +3,7 @@ from flask import jsonify, request
 from app.forms.book import SearchForm
 from app.libs.helper import is_isbn_or_key
 from app.spider.yushu_book import YuShuBook
-from app.view_models.book import BookViewModel
+from app.view_models.book import BookViewModel, BookCollection
 from . import web
 
 
@@ -15,17 +15,19 @@ def search():
     ?q=金庸&page=1
     '''
     form = SearchForm(request.args)
+    books = BookCollection()
     if form.validate():
         q = form.q.data.strip()
         page = form.page.data
         isbn_or_key = is_isbn_or_key(q)
         if isbn_or_key == 'isbn':
-            result = YuShuBook.search_by_isbn(q)
-            result = BookViewModel.package_single(result,q)
+            yushu_book = YuShuBook()
+            yushu_book.search_by_isbn(q)
         else:
-            result = YuShuBook.search_by_keyword(q,page)
-            result = BookViewModel.package_collection(result,q)
-            #dict序列化 API
-        return jsonify(result)
+            yushu_book = YuShuBook()
+            yushu_book.search_by_keyword(q,page)
+        books.fill(yushu_book,q)
+        #dict序列化 API
+        return jsonify(books)
     else:
         return jsonify(form.errors)
