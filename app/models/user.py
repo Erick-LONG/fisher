5f064d26c279f13d79b01b0a4728563504c1583e
erick <834424581@qq.com>
2018-04-22 17:47:51 +0800

    flask-login设置写入浏览器cookie

--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,14 @@
 from app.models.base import Base
 from sqlalchemy import Column,Integer,String,Boolean,Float
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
 
-class User(Base):
+class User(UserMixin,Base):
     id = Column(Integer,primary_key=True)
     nickname = Column(String(24),nullable=True)
     phone_number = Column(String(18),unique=True)
-    _password = Column('password',String(128))
+    _password = Column('password',String(128),nullable=True)
     email = Column(String(50),unique=True,nullable=True)
     confirmed = Column(Boolean,default=False)
     beans = Column(Float,default=0)
@@ -23,3 +24,9 @@ class User(Base):
     @password.setter
     def password(self,raw):
         self._password = generate_password_hash(raw)
+
+    def check_password(self,raw):
+        return check_password_hash(self._password,raw)
+
+    # def get_id(self):
+    #     return self.id
