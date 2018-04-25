447fb4d1e68caf3f002bbb5372cd409a13be8080
erick <834424581@qq.com>
2018-04-26 07:37:34 +0800

    获取最近书籍排序，去重，并利用model添加业务逻辑返回数据

--- app/models/gift.py
+++ app/models/gift.py
@@ -1,6 +1,10 @@
+from flask import current_app
+
 from app.models.base import Base
-from sqlalchemy import Column, Integer, String, Boolean, Float, ForeignKey, SmallInteger
+from sqlalchemy import Column, Integer, String, Boolean, Float, ForeignKey, SmallInteger,desc
 from sqlalchemy.orm import relationship
+
+from app.spider.yushu_book import YuShuBook
 
 
 class Gift(Base):
@@ -11,3 +15,24 @@ class Gift(Base):
     # book = relationship('Book')
     # bid = Column(Integer, ForeignKey('book.id'))
     launched = Column(Boolean,default=False)
+
+    @property
+    def book(self):
+        yushu_book = YuShuBook()
+        yushu_book.search_by_isbn(self.isbn)
+        return yushu_book.first
+
+    #对象代表一个礼物，具体
+    #类代表礼物这个事务，他是抽象，不是具体的一个
+    @classmethod
+    def recent(cls):
+        #链式调用
+        #主体 query
+        #子函数
+        #first all
+        recent_gift = Gift.query.filter_by(
+            launched=False).group_by(
+            Gift.isbn).order_by(
+            desc(Gift.create_time)).limit(
+            current_app.config['RECENT_BOOK_COUNT']).distinct().all()
+        return recent_gift
