839e999a5fee8b5bf08a865cabde452928b2a31a
erick <834424581@qq.com>
2018-04-28 07:32:06 +0800

    规范化命名

--- app/view_models/trade.py
+++ app/view_models/trade.py
@@ -1,3 +1,5 @@
+from app.view_models.book import BookViewModel
+
 
 class TradeInfo:
     def __init__(self,goods):
@@ -19,3 +21,30 @@ class TradeInfo:
             time = time,
             id = single.id
         )
+
+
+class MyTrades:
+    def __init__(self,trade_of_mine,trade_count_list):
+        self.trades = []
+        self.__trade_of_mine = trade_of_mine
+        self.__trade_count_list = trade_count_list
+        self.trades = self.__parse()
+
+    def __parse(self):
+        temp_trades = []
+        for trade in self.__trade_of_mine:
+            my_trade = self.__matching(trade)
+            temp_trades.append(my_trade)
+        return temp_trades
+
+    def __matching(self,trade):
+        count = 0
+        for trade_count in self.__trade_count_list:
+            if trade.isbn == trade_count['isbn']:
+                count = trade_count['count']
+        r = {
+            'wishes_count':count,
+            'book':BookViewModel(trade.book),
+            'id':trade.id
+        }
+        return r
