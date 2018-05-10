31174063a92944e8e39cb71360a3a3856191e2a6
erick <834424581@qq.com>
2018-05-10 22:25:45 +0800

    向他人赠送书籍

--- app/web/wish.py
+++ app/web/wish.py
@@ -1,7 +1,9 @@
 from flask import flash, redirect, url_for, render_template
 from flask_login import login_required, current_user
 
+from app.libs.email import send_mail
 from app.models.base import db
+from app.models.gift import Gift
 from app.models.wish import Wish
 from app.view_models.trade import MyTrades
 from app.view_models.wish import MyWishes
@@ -38,10 +40,22 @@ def save_to_wish(isbn):
 
 
 @web.route('/satisfy/wish/<int:wid>')
+@login_required
 def satisfy_wish(wid):
-    pass
+    wish = Wish.query.get_or_404(wid)
+    gift = Gift.query.filter_by(uid = current_user.id,isbn = wish.isbn).first()
+    if not gift:
+        flash('你还没上传此书')
+    else:
+        send_mail(wish.user.email,'有人想送你一本书','email/satisify_wish.html',wish = wish,gift = gift)
+        flash('已经向他发送了一封邮件')
+    return redirect(url_for('web.book_detail',isbn = wish.isbn))
 
 
 @web.route('/wish/book/<isbn>/redraw')
+@login_required
 def redraw_from_wish(isbn):
-    pass
+    wish = Wish.query.filter_by(isbn=isbn, launched=False).first_or_404()
+    with db.auto_commit():
+        wish.delete()
+    return redirect(url_for('web.my_wish'))
